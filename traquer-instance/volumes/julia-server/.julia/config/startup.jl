bb1b0ea67a0818c7b365f73ef551e07ace5bef8e
Vincent Laugier <vincent.laugier@tekliko.com>
2023-07-05 21:29:09 +1100

    stop tracking startup.jl and traquer-frontend-angular.nginx.conf

--- traquer-instance/volumes/julia-server/.julia/config/startup.jl
+++
@@ -1,4 +0,0 @@
-ENV["TRAQUER_CONFIGURATION_FILE"]="/home/traquer/CODE/TRAQUER.jl/conf/remote-dev-vincent.conf"
-ENV["TRAQUER_CUSTOM_MODULE_DEFINITION_FILE"]="/home/traquer/CODE/TRAQUER.jl/custom/CHTNoumea/src/CHTNoumea-def.jl"
-ENV["TRAQUER_CUSTOM_MODULE_IMPLEMENTATION_FILE"]="/home/traquer/CODE/TRAQUER.jl/custom/CHTNoumea/src/CHTNoumea-imp.jl"
-ENV["TRAQUER_ADD_ADDITIONAL_PROCS"]="false"
