1e37b86e14ddd104e0186db62375730a9e520356
Vincent Laugier <vincent.laugier@tekliko.com>
2023-10-10 14:20:22 +1100

    do not recreate

--- traquer-instance/run-docker-container.sh
+++ traquer-instance/run-docker-container.sh
@@ -4,4 +4,5 @@
 #docker run --name predict-accident-r -itd -p 8787:8787 -e USER=username -e PASSWORD=password stream-techs/predict-accident-r:v1
 
 # With docker-compose
-docker compose --compatibility up --force-recreate
+#docker compose --compatibility up --force-recreate
+docker compose --compatibility up
